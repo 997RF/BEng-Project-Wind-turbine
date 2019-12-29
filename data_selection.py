1b604c53017bf95d3b3d153d94e0fd556ecc5764
Reinis Fisers <56356576+997RF@users.noreply.github.com>
2019-12-29 19:45:26 +0000

    Create data_selection.py
    Selecting data from each month and combining into a one complete file that will be used for description part.

---
+++ data_selection.py
@@ -0,0 +1,15 @@
+###Selecting data###
+
+###Filtering out the unnecessary columns###
+
+f = pd.read_csv("C:/Users/Reinis Fisers/Desktop/University Year 4/Individual Project/Extracted data/2018-06.csv")
+keep_col = ['TimeStamp', 'WindSpeed_mps', 'WindSpeed1', 'WindSpeed2', 'WindSpeed3', 'Pitch_Deg', 'AmbTemp_DegC',
+            'NacelleOrientation_Deg']
+new_f = f[keep_col]
+new_f.to_csv("2018-06f.csv", index=False)
+
+### Comibining all the files into one###
+
+# files = glob.glob("C:/Users/Reinis Fisers/Desktop/University Year 4/Individual Project/filtered data/*.csv")
+# df = pd.concat((pd.read_csv(f, header = 0) for f in files))
+# df.to_csv("combined.csv")
