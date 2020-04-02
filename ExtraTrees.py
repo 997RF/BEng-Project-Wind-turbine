b8ba540bf6835cf61ed1548510172132f8059246
Reinis Fisers <56356576+997RF@users.noreply.github.com>
2020-04-02 18:01:57 +0300

    Update ExtraTrees.py

--- ExtraTrees.py
+++ ExtraTrees.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.ensemble import ExtraTreesClassifier
 
-df = pd.read_csv("afterisoforrest3.csv", index_col=None)
-df = df.head(10000)
+# Loading the data
+df = pd.read_csv("HalfYearFilteredNoNAN.csv", index_col=None)
+df = df.head(200000)
+# Seperating the dependent and independent variables
 dfs = df[['Power_kW', 'WindSpeed_mps', 'WindSpeed1', 'WindSpeed2', 'WindSpeed3', 'AmbTemp_DegC', 'Pitch_Deg', 'NacelleOrientation_Deg',
           'RotorSpeedAve', 'MeasuredYawError', 'Volts1_Vrms', 'Volts2_Vrms', 'Volts3_Vrms', 'Current1_Arms',
           'Current2_Arms', 'Current3_Arms']]
@@ -12,15 +14,23 @@ y = dfs['Power_kW']
 X = dfs.drop('Power_kW', axis=1)
 
 extra_tree_forest = ExtraTreesClassifier(n_estimators=5, criterion='entropy', max_features=2)
+
+# Training the model
 extra_tree_forest.fit(X, y)
+
+# Computing the importance of each feature
 feature_importance = extra_tree_forest.feature_importances_
+
+# Normalizing the individual importances
 feature_importance_normalized = np.std([tree.feature_importances_ for tree in
                                         extra_tree_forest.estimators_],
                                        axis=0)
-indices = np.argsort(feature_importance)[::-1]
-plt.figure(figsize=(10, 10))
-plt.bar(X.columns, feature_importance[indices], yerr=feature_importance_normalized[indices], align="center")
-plt.ylabel('Feature Importances',  weight='bold', size=16)
+indices = np.argsort(feature_importance_normalized)[::-1]
+
+# Plotting a Bar Graph to compare the models
+plt.figure(figsize=(10,8))
+plt.bar(X.columns[indices], feature_importance_normalized[indices], align="center")
 plt.xticks(rotation=90)
-plt.title('Comparison of different Feature Importances',  weight='bold', size=16)
-plt.savefig("ExtraTrees.png", bbox_inches='tight')
+plt.ylabel('Feature Importances')
+plt.title('Comparison of Feature Importances', weight='bold', size=16)
+plt.savefig("Final Extra Trees", bbox_inches='tight')
