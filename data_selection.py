28b956e73744cac4e85327fe7c41b7b2389df74b
Reinis Fisers <56356576+997RF@users.noreply.github.com>
2020-04-16 15:40:02 +0300

    Update data_selection.py

--- data_selection.py
+++ data_selection.py
@@ -1,4 +1,3 @@
-###Selecting data###
 import pandas as pd
 import sklearn
 import glob
@@ -6,16 +5,15 @@ import os
 import csv
 import matplotlib.pyplot as plt
 
-###Filtering out the unnecessary columns###
-
+### Filtering out the unnecessary columns ###
 f = pd.read_csv("C:/Users/Reinis Fisers/Desktop/University Year 4/Individual Project/Extracted data/2018-06.csv")
-keep_col = ['TimeStamp', 'WindSpeed_mps', 'WindSpeed1', 'WindSpeed2', 'WindSpeed3', 'Pitch_Deg', 'AmbTemp_DegC',
-            'NacelleOrientation_Deg']
+keep_col = ['Power_kW', 'WindSpeed_mps', 'WindSpeed1', 'WindSpeed2', 'WindSpeed3', 'AmbTemp_DegC', 'Pitch_Deg', 'NacelleOrientation_Deg',
+          'RotorSpeedAve', 'MeasuredYawError', 'Volts1_Vrms', 'Volts2_Vrms', 'Volts3_Vrms', 'Current1_Arms',
+          'Current2_Arms', 'Current3_Arms']
 new_f = f[keep_col]
 new_f.to_csv("2018-06f.csv", index=False)
 
-### Comibining all the files into one###
-
-# files = glob.glob("C:/Users/Reinis Fisers/Desktop/University Year 4/Individual Project/filtered data/*.csv")
-# df = pd.concat((pd.read_csv(f, header = 0) for f in files))
-# df.to_csv("combined.csv")
+### Comibining all the files into one ###
+files = glob.glob("C:/Users/Reinis Fisers/Desktop/University Year 4/Individual Project/filtered data/*.csv")
+df = pd.concat((pd.read_csv(f, header = 0) for f in files))
+df.to_csv("combined.csv")
