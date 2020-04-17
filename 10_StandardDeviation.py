063e43ef5d5ea937ac016e4d45fab67396700919
Reinis Fisers <56356576+997RF@users.noreply.github.com>
2020-04-17 09:55:54 +0300

    Update 10_StandardDeviation.py

--- 10_StandardDeviation.py
+++ 10_StandardDeviation.py
@@ -1,20 +1,20 @@
 import numpy as np
 import pandas as pd
 
-#import data
+### Import data ###
 data = pd.read_csv("C:/Users/Reinis Fisers/PycharmProjects/TF_TEST/COMBI_Replaceding.csv")
 x = data['WindSpeed_mps']
 
-#Determine mean and std
+### Determine mean and std ###
 data_std = float(np.std(x))
 data_mean = float(np.mean(x))
 anomaly_cut_off = float(data_std * 3)
 
-#Determine the cut-off limits
+### Determine the cut-off limits ###
 lower_limit = data_mean - anomaly_cut_off
 upper_limit = data_mean + anomaly_cut_off
 
-#Remove lines that contain data outside the limits
+### Remove lines that contain data outside the limits ###
 indexNames = data[(data['WindSpeed_mps'] > upper_limit) | (data['WindSpeed_mps'] < lower_limit)].index
 data.drop(indexNames, inplace=True)
 data.to_csv("STDDEVwinspeed.csv")
